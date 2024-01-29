9f29643b54258c84ed87bd3dd65558f8cec258b5
rhoeunbin <rhoeb21@gmail.com>
2024-01-29 21:39:27 +0900

    💡add : Toekn Refresh 기능 정리

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,6 +12,27 @@ export class AuthService {
     private readonly usersService: UsersService,
   ) {}
   /**
+   * 토큰을 사용하게 되는 방식
+   *
+   * 1) 사용자가 로그인 또는 회원가입을 집행하면 accessToken과 refreshToken을 발급 받음
+   * 2) 로그인 할 때는 Basic 토큰과 함께 요청을 보냄
+   *   Basic 토큰은 '이메일:비밀번호'를 Basi64로 인코딩한 형태
+   *   ex) {authorization: 'Basic {token}'}
+   * 3) 아무나 접근할 수 없는 정보 (private route)를 접근할 때는
+   *   accessToken을 Header에 추가해서 요청과 함께 보냄
+   *   ex) {authorization: 'Bearer {token}'}
+   * 4) 토큰과 요청을 함께 받은 서버는 토큰 검증을 통해 현재 요청을 보낸 사용자가 누군지 알 수 있음
+   *   ex) 현재 로그인한 사용자가 작성한 포스트만 가져오려면 토큰의 sub 값에 입력되어 있는 사용자의 포스트만 따로 필터링 할 수 있음
+   *   => 특정 사용자의 토큰이 없다면 다른 사용자의 데이터를 접근 못함
+   * 5) 모든 토큰은 만료 기간이 있음. 만료 기간이 지나면 새로 토큰을 발급받아야함
+   *   그렇지 않으면 jwtService.verify()에서 인증이 통과 안 됨
+   *   그러니 access토큰을 새로 발급 받을 수 있는 /auth/token/access와 refresh토큰을 새로 발급 받을 수 있는 /auth/token/refresh가 필요
+   * 6) 토큰이 만료되면 각각의 토큰을 새로 발급 받을 수 있는 엔드포인트에 요청을 해서 새로운 토큰을 발급받고
+   *   새로운 토큰을 사용해서 private route에 접근한다.
+   */
+
+  /**
+   * 만드려는 기능
    * 1) registerWithEmail
     - email, nickname, password를 입력받고 사용자를 생성
     - 생성이 완료되면 accessToekn과 refreshToken을 반환, 회원가입 후 다시 로그인해주세요 ← 이런 쓸데없는 과정을 방지하기 위해
