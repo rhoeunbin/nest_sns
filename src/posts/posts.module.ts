36cb1d2b0528b5ae7613e319ce83f7445b5f393f
rhoeunbin <rhoeb21@gmail.com>
2024-03-13 22:43:43 +0900

    🔧 fix : 의존성 주입 에러 해결

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,12 +3,17 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModel } from './entities/posts.entity';
+import { AuthService } from 'src/auth/auth.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       PostsModel, // typeorm이 알아서 post의 repository를 만들어줌
     ]),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [PostsController],
   providers: [PostsService],
