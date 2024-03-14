9f6e3c85539f2e084eb5f185d690aa3ee2eeb067
rhoeunbin <rhoeb21@gmail.com>
2024-03-14 18:24:29 +0900

    🔧 fix : verifytoken 메세지 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -101,9 +101,13 @@ export class AuthService {
 
   // 토큰 검증
   verifyToken(token: string) {
-    return this.jwtService.verify(token, {
-      secret: JWT_SECRET,
-    });
+    try {
+      return this.jwtService.verify(token, {
+        secret: JWT_SECRET,
+      });
+    } catch (e) {
+      throw new UnauthorizedException('토큰이 만료됐거나 잘못된 토큰입니다.');
+    }
   }
 
   // 토큰이 만료된 뒤에 refreshtoken 던져주기
