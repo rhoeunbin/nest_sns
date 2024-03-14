e7133732d1f459ec592e0f90fa48cc0bafbba2b2
rhoeunbin <rhoeb21@gmail.com>
2024-03-14 18:46:09 +0900

    💡 feat : use data parameter

--- src/users/decorator/user.decorator.ts
+++ src/users/decorator/user.decorator.ts
@@ -3,16 +3,24 @@ import {
   InternalServerErrorException,
   createParamDecorator,
 } from '@nestjs/common';
+import { UsersModel } from '../entities/users.entity';
 
-export const User = createParamDecorator((data, context: ExecutionContext) => {
-  const req = context.switchToHttp().getRequest();
+export const User = createParamDecorator(
+  (data: keyof UsersModel | undefined, context: ExecutionContext) => {
+    const req = context.switchToHttp().getRequest();
 
-  const user = req.user;
+    const user = req.user as UsersModel;
 
-  if (!user) {
-    throw new InternalServerErrorException(
-      'Request에 user 프로퍼티가 존재하지 않는다',
-    );
-  }
-  return user;
-});
+    if (!user) {
+      throw new InternalServerErrorException(
+        'Request에 user 프로퍼티가 존재하지 않는다',
+      );
+    }
+
+    if (data) {
+      return user[data];
+    }
+
+    return user;
+  },
+);
