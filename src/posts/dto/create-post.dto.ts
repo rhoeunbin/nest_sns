d6680f95f78de63daf3cc7eccea03fe7b4842a66
rhoeunbin <rhoeb21@gmail.com>
2024-03-20 23:53:08 +0900

    🔧 fix : class validator 오류메세지 변경

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,9 +1,13 @@
 import { IsString } from 'class-validator';
 
 export class createPostDto {
-  @IsString()
+  @IsString({
+    message: 'title은 string 타입을 입력해줘야합니다',
+  })
   title: string;
 
-  @IsString()
+  @IsString({
+    message: 'content은 string 타입을 입력해줘야합니다',
+  })
   content: string;
 }
